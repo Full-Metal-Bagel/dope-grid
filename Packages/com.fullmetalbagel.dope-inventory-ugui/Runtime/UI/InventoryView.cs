2fcc9696e7b56c91192d1b5b392266c866a02336
quabug <quabug@gmail.com>
2025-09-22 19:22:00 +0800

    drag controller

--- Packages/com.fullmetalbagel.dope-inventory-ugui/Runtime/UI/InventoryView.cs
+++ Packages/com.fullmetalbagel.dope-inventory-ugui/Runtime/UI/InventoryView.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 namespace DopeGrid.Inventory
 {
     [RequireComponent(typeof(RectTransform))]
-    public class InventoryView : UIBehaviour
+    public class InventoryView : UIBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         [SerializeField] private Vector2 _cellSize = new(64f, 64f);
 
@@ -18,6 +18,7 @@ namespace DopeGrid.Inventory
         private IInventoryItemViewPool _pool = null!;
 
         private readonly Dictionary<int, Image> _itemViews = new(); // instanceId -> Image
+        private DragController _dragController = null!;
         private RectTransform Rect => (RectTransform)transform;
 
         internal Inventory Inventory => _inventory;
@@ -36,9 +37,10 @@ namespace DopeGrid.Inventory
             _inventory = inventory;
             _definitions = definitions;
             _pool = pool ?? new DefaultInventoryItemViewPool();
-
             Debug.Assert(_inventory.IsCreated, this);
             Rect.sizeDelta = new Vector2(_inventory.Width * _cellSize.x, _inventory.Height * _cellSize.y);
+
+            _dragController = new DragController(this);
         }
 
         public void Update()
@@ -74,6 +76,7 @@ namespace DopeGrid.Inventory
 
                 var image = GetOrCreateItemView(instanceId);
                 image.sprite = itemUI.Image;
+                image.raycastTarget = false;
                 image.preserveAspect = false;
 
                 // Compute target AABB (rotated shape) and pre-rotation rect size
@@ -163,6 +166,123 @@ namespace DopeGrid.Inventory
                 RotationDegree.Clockwise270 => new Vector2(0f, -w),
                 _ => Vector2.zero
             };
+        }
+
+        private bool TryGetSprite(Guid definitionId, out Sprite? sprite)
+        {
+            if (_definitions.TryGetValue(definitionId, out var ui) && ui != null && ui.Image != null)
+            {
+                sprite = ui.Image;
+                return true;
+            }
+            sprite = null;
+            return false;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData) => _dragController.OnBeginDrag(eventData);
+        public void OnDrag(PointerEventData eventData) => _dragController.OnDrag(eventData);
+        public void OnEndDrag(PointerEventData eventData) => _dragController.OnEndDrag(eventData);
+
+        private sealed class DragController : IDisposable
+        {
+            private readonly InventoryView _view;
+            private Image _ghost;
+            private int _dragItemIndex = -1;
+
+            public DragController(InventoryView view)
+            {
+                _view = view;
+                _ghost = view._pool.Get();
+#if UNITY_EDITOR
+                _ghost.name = "__ghost__";
+#endif
+                _ghost.transform.SetParent(_view.GetComponentInParent<Canvas>().transform, false);
+                var rt = (RectTransform)_ghost.transform;
+                EnsureTopLeftAnchors(rt);
+                _ghost.preserveAspect = false;
+                _ghost.raycastTarget = false;
+                _ghost.gameObject.SetActive(false);
+            }
+
+            public void Dispose()
+            {
+                if (_ghost != null)
+                {
+                    _view._pool.Release(_ghost);
+                    _ghost = null!;
+                }
+            }
+
+            public void OnBeginDrag(PointerEventData eventData)
+            {
+                var inventory = _view.Inventory;
+                if (!inventory.IsCreated) return;
+
+                if (!TryGetPointerLocalTopLeft(eventData, out var fromTopLeft)) return;
+
+                var gx = Mathf.FloorToInt(fromTopLeft.x / _view.CellSize.x);
+                var gy = Mathf.FloorToInt(fromTopLeft.y / _view.CellSize.y);
+                if (gx < 0 || gy < 0 || gx >= inventory.Width || gy >= inventory.Height) return;
+
+                var idx = inventory.GetItemAt(new int2(gx, gy));
+                if (idx < 0) return;
+                if (!inventory.TryGetItem(idx, out var item)) return;
+                if (!_view.TryGetSprite(item.DefinitionId, out var sprite)) return;
+
+                _dragItemIndex = idx;
+
+                var rotatedSize = new Vector2(item.Shape.Width * _view.CellSize.x, item.Shape.Height * _view.CellSize.y);
+                var preRotSize = item.Rotation is RotationDegree.Clockwise90 or RotationDegree.Clockwise270
+                    ? new Vector2(rotatedSize.y, rotatedSize.x)
+                    : rotatedSize;
+                var angleZ = InventoryView.GetZRotation(item.Rotation);
+
+                if (_ghost == null) return;
+                var grt = (RectTransform)_ghost.transform;
+                grt.sizeDelta = preRotSize;
+                grt.localEulerAngles = new Vector3(0f, 0f, angleZ);
+                grt.anchoredPosition = new Vector2(fromTopLeft.x, -fromTopLeft.y);
+                _ghost.sprite = sprite;
+                _ghost.raycastTarget = false;
+                _ghost.gameObject.SetActive(true);
+                grt.SetAsLastSibling();
+            }
+
+            public void OnDrag(PointerEventData eventData)
+            {
+                if (_dragItemIndex < 0) return;
+                if (_ghost == null) return;
+                if (!TryGetPointerLocalTopLeft(eventData, out var fromTopLeft)) return;
+                var grt = (RectTransform)_ghost.transform;
+                grt.anchoredPosition = new Vector2(fromTopLeft.x, -fromTopLeft.y);
+            }
+
+            public void OnEndDrag(PointerEventData eventData)
+            {
+                _dragItemIndex = -1;
+                if (_ghost != null)
+                {
+                    _ghost.gameObject.SetActive(false);
+                }
+            }
+
+            private bool TryGetPointerLocalTopLeft(PointerEventData eventData, out Vector2 fromTopLeft)
+            {
+                var rectT = _view.RectTransform;
+                var cam = eventData.pressEventCamera;
+                if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(rectT, eventData.position, cam, out var local))
+                {
+                    fromTopLeft = default;
+                    return false;
+                }
+
+                var rect = rectT.rect;
+                var size = rect.size;
+                var left = -rectT.pivot.x * size.x;
+                var top = (1 - rectT.pivot.y) * size.y;
+                fromTopLeft = new Vector2(local.x - left, top - local.y);
+                return true;
+            }
         }
     }
 }
